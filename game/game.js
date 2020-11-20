937f6eb8bfb0b164c5e752eb5e5a571d7aad9ab2
Rubén González Ortiz <rubgon08@ucm.es>
2020-11-20 17:47:30 +0100

    primer sprite

--- game/game.js
+++ game/game.js
@@ -2,11 +2,21 @@ export default class Game extends Phaser.Scene {
   constructor() {
     super({ key: "main" });
   }
-  preload() {}
+  preload() {
+    this.load.image('pigmalion','./resources/sprites/muneco.jpg')
+  };
 
   create() {
     this.add.text(10, 10, "¡Hola, mundo!", { fontColor: 0xffff00 });
+   var player= this.add.sprite(300,300,'pigmalion');
+
+    this.w= scene.input.keyboard.addKey('W');
+
   }
 
-  update(time, delta) {}
+  update(time, delta) {
+    if(this.w.isDown){
+console.log("me quiero morir");
+    }
+  }
 }
