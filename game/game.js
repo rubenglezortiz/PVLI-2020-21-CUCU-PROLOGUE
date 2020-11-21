1817d3c6e4af85e551ef80806aa39d44b119b079
Rubén González Ortiz <rubgon08@ucm.es>
2020-11-21 11:42:13 +0100

    animacion mal hecha
    Co-Authored-By: Jonandra <63009653+Jonandra@users.noreply.github.com>

--- game/game.js
+++ game/game.js
@@ -3,19 +3,29 @@ export default class Game extends Phaser.Scene {
     super({ key: "main" });
   }
   preload() {
-    this.load.image('pigmalion','./resources/sprites/pigmalion.png')
+    // this.load.image('pigmalion','./resources/sprites/pigmalion.png')
+    this.load.spritesheet(
+      'pigmalion', './resources/sprites/pigmalion.png', { frameWidth: 128, frameHeight: 128  }); // spritesheet 384x512
   };
 
   create() {
    this.t=this.add.text(10, 10, "¡Hola, mundo!", { fontColor: 0xffff00 });
 
    this.player= this.add.sprite(300,300,'pigmalion');
+   
+    this.anims.create({
+    key: 'walk',
+    frames: this.anims.generateFrameNumbers('pigmalion', { start: 0, end: 0 }),
+    frameRate: 20,
+    repeat: 1
+  });
 
     this.w= this.input.keyboard.addKey('W');
     this.a=this.input.keyboard.addKey('A');
     this.s=this.input.keyboard.addKey('S');
     this.d=this.input.keyboard.addKey('D');
 
+      
     // this.cameras.main.startFollow(this.player);
     
   }
@@ -23,17 +33,14 @@ export default class Game extends Phaser.Scene {
   update(time, delta) {
    if(this.w.isDown) this.player.y-=4;
    if(this.a.isDown) this.player.x-=4;
-   if(this.s.isDown) this.player.y+=4;
+   if(this.s.isDown){
+      this.player.y+=4;
+      this.player.anims.play('walk');
+   }
    if(this.d.isDown) this.player.x+=4;
    
   
-    // if(this.w.isDown){
-    //   player.setVelocityX(-160);
-    // }
-    // // if(this.w.isDown){
-    // //   console.log("me quiero morir");
-    // // }
-    // this.console.log('2df');
-    // console.log("kshfeiu");
+  // this.player.anims('standingo_sprite');
+
   }
 }
