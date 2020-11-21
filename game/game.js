99cb148331cf3561d8fe0b8bb1b774b7b6ca482b
Jonandra <63009653+Jonandra@users.noreply.github.com>
2020-11-22 00:36:09 +0100

    dandole fondito y animacion a nuestro juego

--- game/game.js
+++ game/game.js
@@ -3,83 +3,78 @@ export default class Game extends Phaser.Scene {
     super({ key: "main" });
   }
   preload() {
-    this.load.spritesheet(
-      'pigmalion', './resources/sprites/pigmalion.png', { frameWidth: 128, frameHeight: 128  }); // spritesheet 384x512
+    
+    this.load.spritesheet(    
+      'pigmalion', './resources/sprites/pigmalion.png', { frameWidth: 192, frameHeight: 192  });
+       // spritesheet 384x512
+       this.load.image('background', './resources/sprites/background.png');
+       this.load.image('objetovacio', './resources/sprites/objetovacio.png');
   };
 
   create() {
-    this.add.text(10, 10, "¡Hola, mundo!", { fontColor: 0xffff00 });
+    //this.add.text(10, 10, "¡Hola, mundodasddsasafda!", { fontColor: 0xffff00 });
+    let { width, height } = this.sys.game.canvas;
     //como que var hijo de puta que el cleon nos suspender
-  
-   this.player= this.add.sprite(300,300,'pigmalion');
+    let edificio = this.physics.add.sprite(0,175,'objetovacio');
+   this.add.image(700,400,'background');
+   this.player= this.physics.add.sprite(300,300,'pigmalion');
    
     this.anims.create({
     key: 'walk',
-    frames: this.anims.generateFrameNumbers('pigmalion', { start: 0, end: 0 }),
+    frames: this.anims.generateFrameNumbers('pigmalion', { start: 0, end: 12 }),
     frameRate: 20,
     repeat: 1
   });
 
 
-   this.anims.create({
-    key: 'walk',
-    frames: this.anims.generateFrameNumbers('pigmalion', { start: 0, end: 9 }),
-    frameRate: 20,
-    repeat: 0
-  });
+  this.physics.add.collider(this.player, edificio);
  
-  this.player.anims.play('walk');
+  
 
     this.w= this.input.keyboard.addKey('W');
     this.a=this.input.keyboard.addKey('A');
     this.s=this.input.keyboard.addKey('S');
     this.d=this.input.keyboard.addKey('D');
 
-<<<<<<< HEAD
-    this.cameras.main.startFollow(this.player);
-    const width = 1400;
-    const height = 800;
-=======
-      
-    // this.cameras.main.startFollow(this.player);
+   
     
->>>>>>> 8905464b9b356b93825624eb93746318f3b58f09
   }
 
   update(time, delta) {
-   if(this.w.isDown) this.player.y-=4;
-   if(this.a.isDown) this.player.x-=4;
-   if(this.s.isDown){
-      this.player.y+=4;
-      this.player.anims.play('walk');
+  
+    if(this.w.isDown||this.a.isDown||this.s.isDown||this.d.isDown){
+   if(this.w.isDown) {
+     this.player.y -=4;
+     this.player.anims.play('walk', true);
    }
-   if(this.d.isDown) this.player.x+=4;
-   
-<<<<<<< HEAD
+   if(this.a.isDown) {this.player.x -=4;
+    this.player.anims.play('walk', true);
+  }
+   if(this.s.isDown) {
+    
+    this.player.anims.play('walk', true);
+    this.player.y+=4; 
+  } 
+   if(this.d.isDown){ this.player.x+=4;
+    this.player.anims.play('walk', true); 
+  }
+}
+else{
+  this.player.anims.play('walk', false); 
+}
+
    if(this.player.x < 0)  {     
-     this.player.x = width-1;
+     this.player.x = this.sys.game.canvas.width-1;
    }  
    if(this.player.x > 1400)  {    
     this.player.x = 1;
   }
   if(this.player.y < 0)  {    
-    this.player.y = height-1;
+    this.player.y = this.sys.game.canvas.height-1;
   }
-  if(this.player.y > 0)  {   
+  if(this.player.y > 800)  {   
     this.player.y = 1;
   }
-    // if(this.w.isDown){
-    //   player.setVelocityX(-160);
-    // }
-    // // if(this.w.isDown){
-    // //   console.log("me quiero morir");
-    // // }
-    // this.console.log('2df');
-    // console.log("kshfeiu");
-=======
-  
-  // this.player.anims('standingo_sprite');
-
->>>>>>> 8905464b9b356b93825624eb93746318f3b58f09
+   
   }
 }
