513200d45be70d0b7fc971b313f474cf91ba8e34
lolitovd22 <madelino@ucm.es>
2020-11-28 11:50:38 +0100

    Añadido clase pigmalion

--- game/game.js
+++ game/game.js
@@ -1,4 +1,5 @@
 import {cst} from "./cst.js"
+import Pigmalion from "./pigmalion.js"
  
 export default class Game extends Phaser.Scene {
   constructor() {
@@ -19,66 +20,27 @@ export default class Game extends Phaser.Scene {
     //como que var hijo de puta que el cleon nos suspender
     let edificio = this.physics.add.sprite(0,175,'objetovacio');
    this.add.image(700,400,'background');
-   this.player= this.physics.add.sprite(300,300,'pigmalion');
+
+   this.player= new Pigmalion(this, 300, 300, 'pigmalion');
+   this.player.create();
    
-    this.anims.create({
-    key: 'walk',
-    frames: this.anims.generateFrameNumbers('pigmalion', { start: 0, end: 12 }),
-    frameRate: 20,
-    repeat: 1
-  });
+    
 
 
   this.physics.add.collider(this.player, edificio);
  
   
 
-    this.w= this.input.keyboard.addKey('W');
-    this.a=this.input.keyboard.addKey('A');
-    this.s=this.input.keyboard.addKey('S');
-    this.d=this.input.keyboard.addKey('D');
+    
 
    
     
   }
 
   update(time, delta) {
-  
-    if(this.w.isDown||this.a.isDown||this.s.isDown||this.d.isDown){
-   if(this.w.isDown) {
-     this.player.y -=4;
-     this.player.anims.play('walk', true);
-   }
-   if(this.a.isDown) {this.player.x -=4;
-    this.player.anims.play('walk', true);
-  }
-   if(this.s.isDown) {
-    
-    this.player.anims.play('walk', true);
-    this.player.y+=4; 
-  } 
-   if(this.d.isDown){ this.player.x+=4;
-    this.player.anims.play('walk', true); 
-  }
-}
-else{
-  this.player.anims.play('walk', false); 
-}
 
-   if(this.player.x < 0)  {     
-     this.player.x = this.sys.game.canvas.width-1;    
-     
-     this.scene.start(cst.SCENES.SALA1);
-   }  
-   if(this.player.x > 1400)  {    
-    this.player.x = 1;
-  }
-  if(this.player.y < 0)  {    
-    this.player.y = this.sys.game.canvas.height-1;
-  }
-  if(this.player.y > 800)  {   
-    this.player.y = 1;
-  }
-   
-  }
+    this.player.update();
+  
+    
+}
 }
---
+++ game/pigmalion.js
@@ -0,0 +1,80 @@
+class Pigmalion extends Phaser.GameObjects.Sprite{
+    constructor(scene,x,y, type){
+        super(scene, x, y, type);
+        console.log(this);
+        scene.add.existing(this);
+        scene.physics.world.enable(this);
+        this.body.immovable = true;
+        this.body.setCollideWorldBounds(false);
+        //this.anim = type;
+    }
+
+    create(){
+        
+        this.w= this.scene.input.keyboard.addKey('W');
+        this.a=this.scene.input.keyboard.addKey('A');
+        this.s=this.scene.input.keyboard.addKey('S');
+        this.d=this.scene.input.keyboard.addKey('D');
+        console.log (this.anim); 
+        this.scene.anims.create({
+            key: 'walk',
+            frames: this.scene.anims.generateFrameNumbers( 'pigmalion', { start: 0, end: 12 }),
+            frameRate: 20,
+            repeat: -1
+          });
+    }
+
+    update(){
+        
+                //derecha
+                if(this.w.isDown||this.a.isDown||this.s.isDown||this.d.isDown){
+                    if(this.w.isDown) {
+                      this.body.setVelocityY(-300);
+                      this.anims.play('walk', true);
+                    }
+                    if(this.a.isDown) {
+                     this.body.setVelocityX(-300);
+                     this.anims.play('walk', true);
+                   }
+                    if(this.s.isDown) {
+                        this.body.setVelocityY(300);
+                     this.anims.play('walk', true);
+                     
+                   } 
+                    if(this.d.isDown){
+                     this.body.setVelocityX(300);
+                     this.anims.play('walk', true); 
+                   }
+                   
+                 }
+
+
+                 else{
+                    this.body.setVelocity(0,0);
+                   this.scene.player.anims.play('walk', false); 
+                 }
+
+                
+                    if(this.x < 0)  {     
+                      this.player.x = this.sys.game.canvas.width-1;    
+                      
+                      this.scene.start(cst.SCENES.SALA1);
+                    }  
+                    if(this.x > 1400)  {    
+                     this.x = 1;
+                   }
+                   if(this.y < 0)  {    
+                     this.y = this.sys.game.canvas.height-1;
+                   }
+                   if(this.y > 800)  {   
+                     this.y = 1;
+                   }
+                    
+                   
+        
+        
+    }
+
+}
+
+export default Pigmalion;
