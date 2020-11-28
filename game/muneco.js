22bd1e7c8e325210f775801b0848a1461b7d204a
Rubén González Ortiz <rubgon08@ucm.es>
2020-11-28 12:10:12 +0100

    Disparo instanciado
    Se instancia al pulsar la "E" para probarlo. Sólo falta meter sprite y anim (lo haré mañana)

--- game/muneco.js
+++ game/muneco.js
@@ -1,64 +1,16 @@
-import Npc from './npc.js';
+import Npc from "./npc.js";
 
-export default class Muneco  {
-    
-    constructor() {
-      super({ key: "muneco" });
-    }
-
-    preload() {
-       
-    }
-  
-    create() {
-     const  pPersLimit = 100;  
-     this.pPersuasion = 0;
-     this.pVida = 100;
-      
-     //Creación de la variable de tipo NPC, 
-     //habrá que pasarle como parámetros rodo lo que necesite NPC
-
-
-     //Funciones
-     let pierdeVida = function(vidaPerdida){
-        pVida -= vidaPerdida;
-     };
-
-
-     let subirPersuasion = function(puntosPersuasion){
-        pPersiasion += puntosPersuasion;
-     };
-
-     //let deplazar = function(posicionDestino)
-
-
-     let comenzarCombate = function(nombreEscena){
-         //Cargar escena de la batalla
-     }
-
-     //a este método se le llama cada vez que se le da al boton mercy
-     let mercy = function(){
-         
-        if(pPersuasion >= pPersLimit){
-            //se acaba el combate: invocar a los metodos que sean
-        }
-     };
-
-
-    }
-  
-    
-    update(time, delta) {
-      
-    }
+export default class Muneco extends Phaser.Scene {
+  constructor() {
+    super({ key: "muneco" });
   }
 
   preload() {}
 
   create() {
     const pPersLimit = 100;
-    let pPersuasion = 0;
-    let pVida = 100;
+    this.pPersuasion = 0;
+    this.pVida = 100;
 
     //Creación de la variable de tipo NPC,
     //habrá que pasarle como parámetros rodo lo que necesite NPC
@@ -86,5 +38,6 @@ export default class Muneco  {
     };
   }
 
-  update(time, delta) {}
+  update(time, delta) {
+  }
 }
