3744280c4d2bcbc2df629f8ab7626bc6a0ccc7d3
RTobar01 <rtobar01@ucm.es>
2020-12-01 12:52:46 +0100

    Funcionalidad explosiones + temporizador
    Co-Authored-By: rubenglezortiz <61495670+rubenglezortiz@users.noreply.github.com>

--- game/explosion.js
+++ game/explosion.js
@@ -2,7 +2,14 @@ export default class Explosion extends Phaser.GameObjects.Sprite {
     constructor(config) {
       super(config.scene, config.x, config.y, config.type);
       config.scene.add.existing(this);
-      //let timer = this.scene.time.delayedCall(20000, this.destroy());
+      this.timer = 0;
+    }
+    
+    preUpdate(time,delta)
+    {
+      this.timer += delta;
+      if (this.timer > 1000) {
+          this.destroy();
+    }
     }
 }
-  
