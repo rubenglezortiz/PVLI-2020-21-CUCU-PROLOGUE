2fcec53b2c18948effaa78c0e9273654fb5ac5e3
RTobar01 <rtobar01@ucm.es>
2020-12-17 21:55:15 +0100

    Opciones de hablar
    Al darle al botón de hablar, dependiendo del que elijas saldrá un mensaje distinto y los puntos de persuasión cambiarán.

--- game/scenes/menuCombate.js
+++ game/scenes/menuCombate.js
@@ -12,15 +12,15 @@ export default class MenuCombate extends Phaser.Scene {
     this.menu.setAlpha(0.5);
     this.mercy;
 
-    this.attackButton = this.add.sprite(300, 700, "attackButton").setInteractive();
-    this.talkButton = this.add.sprite(700, 700, "talkButton").setInteractive();
+    this.attackButton = this.add.sprite(200, 150, "attackButton").setInteractive();
+    this.talkButton = this.add.sprite(550, 150, "talkButton").setInteractive();
 
     eventsCenter.on("canMercy", this.setMercyButton, this);
     if (!this.mercy) {
-      this.mercyButton = this.add.sprite(1100, 700, "mercyButton");
+      this.mercyButton = this.add.sprite(850, 150, "mercyButton");
       this.mercyButton.setAlpha(0.5);
     } else {
-      this.mercyButton = this.add.sprite(1100, 700, "mercyButton").setInteractive();
+      this.mercyButton = this.add.sprite(850, 150, "mercyButton").setInteractive();
       this.mercyButton.setAlpha(1);
     }
   
@@ -41,7 +41,7 @@ export default class MenuCombate extends Phaser.Scene {
     });
     // Botón de ataque
     this.attackButton.on("pointerdown", (pointer) => { 
-      this.backButton = this.add.sprite(100, 100, "backButton").setInteractive();
+      this.backButton = this.add.sprite(1200, 150, "backButton").setInteractive();
       this.backButton.on("pointerover", (pointer) => {
         this.backButton.setAlpha(0.5);
       });
@@ -67,7 +67,7 @@ export default class MenuCombate extends Phaser.Scene {
           button.setAlpha(0.5);
         });
         this.backButton.setInteractive();
-        this.barraFinal = this.add.sprite(700, 500, "bar");
+        this.barraFinal = this.add.sprite(700, 700, "bar");
     // Primera barra móvil
     this.barramovil1 = new rod({
       scene: this,
@@ -89,9 +89,119 @@ export default class MenuCombate extends Phaser.Scene {
       eventsCenter.emit("damage", this.damage);
       });
     this.talkButton.on("pointerdown", (pointer) => {
-      this.persuasion = 10;
-      eventsCenter.emit("persuade", this.persuasion);
-      this.parar();
+      //this.persuasion = 10;
+      this.buttonVec.forEach((button) => {
+        button.disableInteractive();
+        button.setAlpha(0.5);
+      })
+      this.talkOption1 = this.add.sprite(200, 700, "talkButton").setInteractive();
+      this.talkOption2 = this.add.sprite(550, 700, "talkButton").setInteractive();
+      this.talkOption3= this.add.sprite(850, 700, "talkButton").setInteractive();
+      this.talkOption4 = this.add.sprite(1200, 700, "talkButton").setInteractive();
+      
+      this.talkOptionsVec = [this.talkOption1, this.talkOption2, this.talkOption3,this.talkOption4];
+      this.talkOptionsVec.forEach((button) => {
+        button.on("pointerover", (pointer) => {
+          button.setAlpha(0.5);
+        });
+
+        button.on("pointerout", (pointer) => {
+          button.setAlpha(1);
+        });
+
+        button.on("pointerdown", (pointer) => {
+          this.talkOptionsVec.forEach((option) => {
+            option.setAlpha(0.5);
+            option.disableInteractive();
+            this.buttonVec.forEach((button) => {
+              button.setAlpha(0.5);
+              button.disableInteractive();
+            });
+            this.backButton2.destroy();
+        });
+      });
+    });
+      this.talkOption1.on("pointerdown", (pointer) => {
+        this.persuasion = 10;
+        let text = this.add.text(200,500, "- Has elegido la opción 1, que da 10 de persuasión -");
+        text.setFont("Arial Black");
+        text.setFontSize(50);
+        this.timer = this.time.addEvent({
+          delay: 2000,
+          callback: () => {
+            eventsCenter.emit("persuade", this.persuasion);
+            this.parar();
+          }
+        });
+        });
+        this.talkOption2.on("pointerdown", (pointer) => {
+          this.persuasion = 20;
+          let text = this.add.text(200,500, "- Has elegido la opción 2, que da 20 de persuasión -");
+          text.setFont("Arial Black");
+          text.setFontSize(50);
+          this.timer = this.time.addEvent({
+            delay: 2000,
+            callback: () => {
+              eventsCenter.emit("persuade", this.persuasion);
+              this.parar();
+            }
+  
+          });
+          });
+          this.talkOption3.on("pointerdown", (pointer) => {
+            this.persuasion = -30;
+            let text = this.add.text(200,500, "- Has elegido la opción 3, que da -30 de persuasión -");
+            text.setFont("Arial Black");
+            text.setFontSize(50);
+            this.timer = this.time.addEvent({
+              delay: 2000,
+              callback: () => {
+                eventsCenter.emit("persuade", this.persuasion);
+                this.parar();
+              }
+    
+            });
+            });
+            this.talkOption4.on("pointerdown", (pointer) => {
+              this.persuasion = 0;
+              let text = this.add.text(200,500, "- Has elegido la opción 4, que da 0 de persuasión -");
+              text.setFont("Arial Black");
+              text.setFontSize(50);
+              this.timer = this.time.addEvent({
+                delay: 2000,
+                callback: () => {
+                  eventsCenter.emit("persuade", this.persuasion);
+                  this.parar();
+                }
+      
+              });
+              });
+      this.backButton2 = this.add.sprite(1200,150,"backButton").setInteractive();
+      this.backButton2.on("pointerover", (pointer) => {
+        this.backButton2.setAlpha(0.5);
+      });
+      this.backButton2.on("pointerout",(pointer) => {
+        this.backButton2.setAlpha(1);
+      });
+      this.backButton2.on("pointerdown", (pointer) => {
+        this.talkOptionsVec.forEach((option) => {
+          option.destroy();
+          
+        });
+          this.attackButton.setInteractive();
+          this.attackButton.setAlpha(1);
+          this.talkButton.setInteractive();
+          this.talkButton.setAlpha(1);
+        if (this.mercy)
+          {
+            this.mercyButton.setInteractive();
+            this.mercyButton.setAlpha(1);
+          }
+          this.backButton2.destroy();
+      })
+
+      
+      //this.parar();
     });
     this.mercyButton.on("pointerdown", (pointer) => {
       eventsCenter.emit("isMercy", true);
