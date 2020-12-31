38ad9e31f3860d9a7309f3c1e14abcdfbbe043f2
Rubén González Ortiz <rubgon08@ucm.es>
2020-12-31 12:43:45 +0100

    ataque pomponina

--- game/monecoAttacks/pomponinaAttack.js
+++ game/monecoAttacks/pomponinaAttack.js
@@ -3,17 +3,25 @@ export default class PomponinaAttack extends Phaser.GameObjects.Sprite {
     super(config.scene, config.x, config.y, config.type);
     config.scene.add.existing(this);
     config.scene.physics.add.existing(this);
-    config.scene.physics.world.enable(this);    
-    this.velY = Phaser.Math.Between(-500, 500);
-    this.body.setVelocityY(this.velY);
-    this.body.setVelocityX(-150);
+    config.scene.physics.world.enable(this);
+    this.velocity = 400;
+    this.velY = Phaser.Math.Between(0, 5);
+    console.log(this.velY);
+    this.velX = 1;
+    this.normalize = Math.sqrt(this.velX + Math.pow(this.velY, 2));
+    console.log(this.normalize);
+    this.velX /= this.normalize;
+    this.velY /= this.normalize;
+
+    this.velX *= this.velocity;
+    this.velY *= this.velocity;
+    this.body.setVelocity(this.velX, this.velY);
   }
 
   preUpdate(t, dt) {
     super.preUpdate(t, dt);
-    this.body.setCollideWorldBounds(true);   
+    this.body.setCollideWorldBounds(true);
     this.body.setBounce(1);
-    if (this.x < this.width/2 +1) this.destroy();
-    // console.log(this.x,"  ",this.width);
+    if (this.x < this.width / 2 + 1) this.destroy();
   }
 }
