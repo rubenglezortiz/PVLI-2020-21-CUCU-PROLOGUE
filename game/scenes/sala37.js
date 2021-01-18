9a023e77492bb13919e8ee798d5270fa34dee32f
RTobar01 <rtobar01@ucm.es>
2021-01-18 11:36:06 +0100

    Revert "Diálogo inicial con los tres muñecos"
    This reverts commit d065a0969d3fdb1a0608ff25c41660da4a1bc2c7.

--- game/scenes/sala37.js
+++ game/scenes/sala37.js
@@ -1,7 +1,7 @@
 import SalaBase from "./sala_base.js";
 import {cst}from "./cst.js";
 import Prop from "../gameobject/prop.js";
-import eventsCenter from "../eventsCenter.js"
+
 export default class Sala37 extends SalaBase{
     constructor(){
       super(cst.SCENES.SALA37, [0, 0, 0, cst.SCENES.SALA36],false)
@@ -14,7 +14,7 @@ export default class Sala37 extends SalaBase{
       this.camino= this.add.image(this.sys.game.canvas.width/2+90,this.sys.game.canvas.height+350,"pomponinacamino1")
       this.camino.angle=90;
       if (this._runInfo._monecos[2] === 0) {
-        this.pomponina=new Prop(this,1200,this.sys.game.canvas.height/2,"pomponina_idl",this.player,60,50,true, this.hablar, this);
+        this.pomponina=new Prop(this,1200,this.sys.game.canvas.height/2,"pomponina_idl",this.player,60,50,true, this.comenzarCombate, this);
         this.pomponina.play("pomponina_idl",true);  
         this.e = this.input.keyboard.addKey("E");    
       }
@@ -25,16 +25,7 @@ export default class Sala37 extends SalaBase{
         super.update();        
       }
    
-      hablar = function () {
-        eventsCenter.emit("thisKey", this._nombreSala);
-        this.scene.launch("dialogo", {
-          npc: "pomponina",
-          prevKey: cst.SCENES.SALA37,
-          objs: this.objetos,
-        });
-        this.scene.pause();
-        this.player.resetInput();
-      }
+    
     comenzarCombate = function(){
       
       if (this._runInfo._monecos[2] === 0){
