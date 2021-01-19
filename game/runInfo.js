029af7f54207d536aa9c7e4fec9452a6835d58cd
Rubén González Ortiz <rubgon08@ucm.es>
2021-01-19 20:23:02 +0100

    run info a 0

--- game/runInfo.js
+++ game/runInfo.js
@@ -1,6 +1,6 @@
 export default class RunInfo{
     //0->nada 1->muerto 2->salvado
-    _monecos = [2,2,1]
+    _monecos = [0,0,0]
     _props_interactuables = [0,0,0,0,0,0,0,0,0,0,0]
 
     //orden de props interactuables: 0:fuente ,1:jarrón,2:cómoda,3:cliente(sala34),4:cliente(sala32),5:florista
